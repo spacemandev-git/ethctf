89dda225376e6ef5a5db3458319ab3a056926d93
Jordan Earls <earlz@earlz.net>
2019-02-16 01:12:19 -0700

    Refactor quest interface to separate file

--- contracts/QuestTemplate.sol
+++ contracts/QuestTemplate.sol
@@ -1,16 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "./QuestInterface.sol";
 
-interface QuestInterface{
-    function getQuestName() external pure returns (string memory name);
-    function getStepInfo() external view returns (string memory url);
-    function isQuestComplete(address hacker) external view returns (bool complete);
-    function beginQuest(address hacker) external returns (bool success); //only usable by score engine
-    function spawnStep() external returns (address stepAddress);
-    function stepsRemaining(address hacker) external view returns (uint steps);
-    function testStep() external returns (bool);
-    function getStep() external view  returns (address step);
-}
 contract QuestTemplate is QuestInterface{
     address scoringEngine;
     mapping (address => uint) hackerStep;
