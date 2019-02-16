723d4bb696c8db2b21c012d7fd7091159745942d
Jordan Earls <earlz@earlz.net>
2019-02-15 17:02:40 -0700

    Update quest interface to include step info

--- contracts/QuestTemplate.sol
+++ contracts/QuestTemplate.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 
-interface QuestTemplate{
+interface QuestInterface{
     function getQuestName() view external returns (string memory name);
+    function getStepInfo(address hacker) view external returns (string memory url);
     function testQuest(address instance) external returns (bool success);
     function spawnNextStep(address hacker) external returns (address stepAddress);
     function stepComplete(address stepAddress) external returns (bool complete);
