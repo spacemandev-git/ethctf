640b3a78cc7ea0d7903a98e6ee36b8682196a25e
Jordan Earls <earlz@earlz.net>
2019-02-17 02:00:45 -0700

    Adapt into final QuestInterface

--- src/quests/XPLToken/Step1Test.sol
+++ src/quests/XPLToken/Step1Test.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.5.0;
 
-contract XPLTokenQuest{
-    //metadata goes in here somehow... 
+interface QuestInterface{
+  function testStep(uint step, RootQuestInstance instance) external returns (bool);
+}
+interface RootQuestInstance{
+    function getStepContract(uint id) external view returns (address);
+    function getHacker() external view returns (address);
+}
 
+contract XPLTokenQuest is QuestInterface{
+    //metadata goes in here somehow... 
+    function testStep(uint step, RootQuestInstance instance) external returns (bool){
+        return testStep1(XPLToken(instance.getStepContract(0)), instance.getHacker());
+    }
     function testStep1(XPLToken token, address hacker) internal returns (bool){
         return token.testWinner(hacker);
     }    
